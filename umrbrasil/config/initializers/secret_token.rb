2ef13735e98534ce07b873fb29d4450805f926cb
Vagner Zampieri <vgzampieri@gmail.com>
2012-04-23 15:03:18 -0300

    Add project UMRBrasil and change files

---
+++ umrbrasil/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Umrbrasil::Application.config.secret_token = '3c6bc4a0db840607c492bcaa173007a8de8168fc4ab6de835bfcf50aedcdd83f3852bda82c44220c9ab00fe937452ce701a199daff85f6e0295d6c724d8bd4f7'
