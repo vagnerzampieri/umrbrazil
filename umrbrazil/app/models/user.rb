84e827f9f8d184f4f12b9d1be349ae6a5a5d3274
Vagner Zampieri <vgzampieri@gmail.com>
2012-04-27 19:47:29 -0300

    Updated Type User

--- umrbrazil/app/models/user.rb
+++ umrbrazil/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+  belongs_to :type_user
+  belongs_to :account
+
   attr_accessible :name, :login, :email
 end
--- umrbrazil/config/routes.rb
+++ umrbrazil/config/routes.rb
@@ -1,4 +1,6 @@
 Umrbrazil::Application.routes.draw do
+  resources :accounts
+
   resources :users
 
   root :to => 'home#index'
