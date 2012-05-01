a2ffb97fd1a91bc1c146652f652ae828a45bd512
Vagner Zampieri <vgzampieri@gmail.com>
2012-04-30 21:56:22 -0300

    Comment attr_accessible

--- umrbrazil/app/models/user.rb
+++ umrbrazil/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   belongs_to :account
   belongs_to :type_user
-  attr_accessible :name, :login, :email
+  #attr_accessible :name, :login, :email, :password, :password_confirmation
 end
