171ddf1967cf26db60c2d4179fa6132de88a8f03
Vagner Zampieri <vgzampieri@gmail.com>
2012-05-05 13:02:32 -0300

    Remove Unit Test

--- umrbrazil/test/unit/type_telephone_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TypeTelephoneTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- umrbrazil/test/unit/type_user_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TypeUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
