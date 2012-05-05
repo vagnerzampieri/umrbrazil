45fb425258591dea17960b47acab8e34acfeeb50
Vagner Zampieri <vgzampieri@gmail.com>
2012-05-05 17:13:05 -0300

    Relationship between accounts and telephones

--- umrbrazil/app/models/account.rb
+++ umrbrazil/app/models/account.rb
@@ -1,3 +1,6 @@
 class Account < ActiveRecord::Base
   attr_accessible :cpf, :address, :number, :complement, :neighborhood, :city, :state, :country
+  has_one :user
+  has_many :accounts_telephones
+  has_many :telephones, :through => :accounts_telephones
 end
--- umrbrazil/app/models/telephone.rb
+++ umrbrazil/app/models/telephone.rb
@@ -1,3 +1,5 @@
 class Telephone < ActiveRecord::Base
   belongs_to :type_telephone
+  has_many :accounts_telephones
+  has_many :accounts, :through => :accounts_telephones
 end
