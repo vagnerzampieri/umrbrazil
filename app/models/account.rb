3cd029fe554f41e2cb228da8e63f40a974f5efea
Vagner Zampieri <vgzampieri@gmail.com>
2012-05-27 22:31:21 -0300

    Rename UsersCompany to AccountsCompany

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,10 @@
 class Account < ActiveRecord::Base
-  attr_accessible :cpf, :address, :number, :complement, :neighborhood, :city, :state, :country
+  image_accessor :cover_image
+  attr_accessible :cpf, :address, :number, :complement, :neighborhood, :city, :state, :country, :cover_image
   has_one :user
+  has_many :accounts_companies
+  has_many :companies, :through => :accounts_companies
+
   has_many :accounts_telephones
   has_many :telephones, :through => :accounts_telephones
 end
