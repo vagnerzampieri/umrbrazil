06ee978e69d32cc32c9aeabd24f6b079c628c8ee
Vagner Zampieri <vgzampieri@gmail.com>
2012-07-22 16:00:33 -0300

    Layout

--- app/assets/javascripts/bootswatch.js
+++ app/assets/javascripts/bootswatch.js
@@ -1,10 +1,10 @@
 $('#main-menu').load('shared/main-menu.html', function(){
-	if($('body').hasClass('preview')){
-		$('#main-menu-left').append('<li class="dropdown" id="preview-menu"></li>');
-		$('#preview-menu').load('shared/preview-menu.html');
-	}
+  if($('body').hasClass('preview')){
+    $('#main-menu-left').append('<li class="dropdown" id="preview-menu"></li>');
+    $('#preview-menu').load('shared/preview-menu.html');
+  }
 
-	$('a[rel=tooltip]').tooltip({
-		'placement': 'bottom'
-	});
+  $('a[rel=tooltip]').tooltip({
+    'placement': 'bottom'
+  });
 });
