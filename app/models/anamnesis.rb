815d3d7f091cb6bad4c835a2e72adbf1a4d9e7be
Vagner Zampieri <vgzampieri@gmail.com>
2012-08-26 15:54:16 -0300

    Tags in Anamnesis

--- app/models/anamnesis.rb
+++ app/models/anamnesis.rb
@@ -1,5 +1,8 @@
 class Anamnesis < ActiveRecord::Base
-  attr_accessible :blood_type, :rh_factor, :weight, :height, :sex, :blood_pressure, :smoking, :drugs, :account_id
+  attr_accessible :blood_type, :rh_factor, :weight, :height, :sex, :blood_pressure, :smoking, :account_id, :diseases, :disease_list, :drugs, :drug_list, :allergies, :allergy_list, :surgeries, :surgery_list
+
+  acts_as_taggable_on :diseases, :drugs, :allergies, :surgeries
+  has_many :taggings, :as => :taggable, :dependent => :destroy, :include => :tag, :class_name => "ActsAsTaggableOn::Tagging"
 
   belongs_to :account
 end
