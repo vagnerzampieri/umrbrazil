46d479e7ed6c26032c7620155ba124da456a9950
Vagner Zampieri <vgzampieri@gmail.com>
2012-09-11 20:44:30 -0300

    Anamnese layout and tag list

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Umrbrazil::Application.routes.draw do
   scope 'admin' do
     resources :conta, controller: :accounts, as: :accounts, path_names: {new: 'adicionar', edit: 'editar'}
     resources :usuarios, controller: :users, as: :users, path_names: {new: 'adicionar', edit: 'editar'}
-    resources :anaminese, controller: :anamneses, as: :anamneses, path_names: {new: 'adicionar', edit: 'editar'}
+    resources :anaminese, controller: :anamneses, as: :anamneses, path_names: {edit: 'editar'}, only: [:edit, :show, :update]
     resources :exames, controller: :exams, as: :exams, path_names: {new: 'adicionar', edit: 'editar'}
     resources :historicos, controller: :histories, as: :histories, path_names: {new: 'adicionar', edit: 'editar'}
     resources :organizacoes, controller: :companies, as: :companies, path_names: {new: 'adicionar', edit: 'editar'}
     get 'painel-principal' => 'dashboard#index', as: :dashboard
   end
 
-    match 'logar'          => 'user_sessions#new',            as: :login
-    match 'deslogar'       => 'user_sessions#destroy',        as: :logout
-    match 'nao-autorizado' => 'user_sessions#not_authorized', as: :not_authorized
-    post 'sessao-usuario'  => 'user_sessions#create',         as: :user_sessions
+  match 'logar'          => 'user_sessions#new',            as: :login
+  match 'deslogar'       => 'user_sessions#destroy',        as: :logout
+  match 'nao-autorizado' => 'user_sessions#not_authorized', as: :not_authorized
+  post 'sessao-usuario'  => 'user_sessions#create',         as: :user_sessions
 
   root :to => 'home#index'
 end
