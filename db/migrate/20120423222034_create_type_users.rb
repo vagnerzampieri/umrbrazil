c3b50efd4fb1ffaf7e7fd5d6eaf02de7c96cffdf
Vagner Zampieri <vgzampieri@gmail.com>
2012-09-12 23:57:26 -0300

    Implement UUID

--- db/migrate/20120423222034_create_type_users.rb
+++ db/migrate/20120423222034_create_type_users.rb
@@ -1,11 +1,10 @@
 # encoding: UTF-8
 class CreateTypeUsers < ActiveRecord::Migration
   def change
-=begin
     execute <<-SQL
       CREATE EXTENSION "uuid-ossp"
     SQL
-=end
+
     create_table :type_users do |t|
       t.string :name
       t.boolean :enabled
--- db/migrate/20120423223634_create_telephones.rb
+++ db/migrate/20120423223634_create_telephones.rb
@@ -1,5 +1,22 @@
 class CreateTelephones < ActiveRecord::Migration
   def change
+    execute <<-SQL
+      CREATE TABLE telephones (
+        id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+        created_at timestamp NOT NULL,
+        updated_at timestamp NOT NULL
+      );
+    SQL
+
+    add_column :telephones, :ddd,               :string
+    add_column :telephones, :tel,               :string
+    add_column :telephones, :type_telephone_id, :integer
+    add_column :telephones, :branch_line,       :string
+    add_column :telephones, :published,         :boolean
+    add_column :telephones, :enabled,           :boolean
+    add_column :telephones, :published_at,      :datetime
+    add_column :telephones, :deleted_at,        :datetime
+=begin
     create_table :telephones do |t|
       t.string :ddd
       t.string :tel
@@ -12,5 +29,6 @@ class CreateTelephones < ActiveRecord::Migration
 
       t.timestamps
     end
+=end
   end
 end
